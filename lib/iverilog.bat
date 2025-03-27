1824d15db8295e67311639ee412c557398be2c67
Kees Krijnen <k.krijnen@home.nl>
2025-03-27 08:45:45 +0100

    Update

--- lib/iverilog.bat
+++ lib/iverilog.bat
@@ -14,26 +14,26 @@ if exist .\bin rmdir /Q/S bin
 if not exist .\bin mkdir bin
 cd .\bin
 if "%1"=="" (
-  iverilog -o equalizer_tb.out -I .. ..\equalizer.v ..\equalizer_tb.sv
-  iverilog -o interpolator_tb.out -I .. ..\interpolator.v ..\interpolator_tb.sv
-  iverilog -o randomizer_tb.out -I .. ..\randomizer.v ..\randomizer_tb.sv
-  iverilog -o sine_wg_cor_tb.out -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
+  iverilog.exe -o equalizer_tb.out -I .. ..\equalizer.v ..\equalizer_tb.sv
+  iverilog.exe -o interpolator_tb.out -I .. ..\interpolator.v ..\interpolator_tb.sv
+  iverilog.exe -o randomizer_tb.out -I .. ..\randomizer.v ..\randomizer_tb.sv
+  iverilog.exe -o sine_wg_cor_tb.out -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
 ) else (
   if "%1"=="VCD" (
-    iverilog -DGTK_WAVE -o equalizer_tb.out -I .. ..\equalizer.v ..\equalizer_tb.sv
-    iverilog -DGTK_WAVE -o interpolator_tb.out -I .. ..\interpolator.v ..\interpolator_tb.sv
-    iverilog -DGTK_WAVE -o randomizer_tb.out -I .. ..\randomizer.v ..\randomizer_tb.sv
-    iverilog -DGTK_WAVE -o sine_wg_cor_tb.out -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
+    iverilog.exe -DGTK_WAVE -o equalizer_tb.out -I .. ..\equalizer.v ..\equalizer_tb.sv
+    iverilog.exe -DGTK_WAVE -o interpolator_tb.out -I .. ..\interpolator.v ..\interpolator_tb.sv
+    iverilog.exe -DGTK_WAVE -o randomizer_tb.out -I .. ..\randomizer.v ..\randomizer_tb.sv
+    iverilog.exe -DGTK_WAVE -o sine_wg_cor_tb.out -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
   ) else (
-    iverilog -I .. ..\equalizer.v ..\equalizer_tb.sv
-    iverilog -I .. ..\interpolator.v ..\interpolator_tb.sv
-    iverilog -I .. ..\randomizer.v ..\randomizer_tb.sv
-    iverilog -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
+    iverilog.exe -I .. ..\equalizer.v ..\equalizer_tb.sv
+    iverilog.exe -I .. ..\interpolator.v ..\interpolator_tb.sv
+    iverilog.exe -I .. ..\randomizer.v ..\randomizer_tb.sv
+    iverilog.exe -I .. ..\sine_wg_cor.v ..\sine_wg_cor_tb.sv
   )
 )
-if exist equalizer_tb.out vvp equalizer_tb.out
-if exist interpolator_tb.out vvp interpolator_tb.out
-if exist randomizer_tb.out vvp randomizer_tb.out
-if exist sine_wg_cor_tb.out vvp sine_wg_cor_tb.out
+if exist equalizer_tb.out vvp.exe equalizer_tb.out
+if exist interpolator_tb.out vvp.exe interpolator_tb.out
+if exist randomizer_tb.out vvp.exe randomizer_tb.out
+if exist sine_wg_cor_tb.out vvp.exe sine_wg_cor_tb.out
 cd ..
 :END
